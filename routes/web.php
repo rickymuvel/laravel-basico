2c407b3f554561f6d945088deeea52a0e03c281e
Luis Muñoz <luenmuvel@gmail.com>
2018-01-15 23:34:17 -0500

    Enviamos todos los archivos iniciales

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo "<a href='contactos'>Contacto</a><br>";
+    echo "<a href='contactos'>Contacto</a><br>";
+    echo "<a href='contactos'>Contacto</a><br>";
+    echo "<a href='contactos'>Contacto</a><br>";
+    echo "<a href='contactos'>Contacto</a><br>";
+});
+
+
+Route::get('contacto', ['as' => 'contactos', function(){
+	return "Sección de contactos";
+}]);
+
+Route::get('saludos/{nombre?}', function($nombre = "Invitado"){
+	return "Saludos $nombre";
+})->where('nombre',"[A-Za-z]+");
